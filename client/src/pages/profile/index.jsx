84b67dfd4c5fa7417e7106cfd5c4c7678a1ac1a6
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-08-08 18:13:59 +0530

    update profile and image addition done

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,26 +1,139 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppStore } from "@/store";
 import { IoArrowBack } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 import { colors, getColors } from "@/lib/utils";
 import { FaPlus, FaTrash } from "react-icons/fa";
 import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { apiClient } from "@/lib/api-client";
+import {
+  ADD_PROFILE_IMAGE_ROUTE,
+  UPDATE_PROFILE_ROUTE,
+  HOST,
+  REMOVE_PROFILE_IMAGE_ROUTE
+} from "@/utils/constants";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
   const navigate = useNavigate();
   const { userInfo, setUserInfo } = useAppStore();
-  const [firstName, setfirstName] = useState("");
+  const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [image, setImage] = useState(undefined);
   const [hovered, setHovered] = useState(false);
   const [selectedColor, setSelectedColor] = useState(0);
+  const fileInputRef = useRef(null);
+
+  useEffect(() => {
+    if (userInfo.profileSetup) {
+      setFirstName(userInfo.firstName);
+      setLastName(userInfo.lastName);
+      setSelectedColor(userInfo.color);
+      console.log(`${HOST}/${userInfo.image}`);
+      
+      if(userInfo.image) setImage(`${HOST}/${userInfo.image}`);
+      else setImage(null);
+    }
+  }, [userInfo]);
+
+  const validateProfile = () => {
+    if (!firstName) {
+      toast.error("First name is required!");
+      return false;
+    }
+    if (!lastName) {
+      toast.error("Last name is required!");
+      return false;
+    }
+    return true;
+  };
+
+  const saveChanges = async () => {
+    if (validateProfile()) {
+      try {
+        const res = await apiClient.post(
+          UPDATE_PROFILE_ROUTE,
+          { firstName, lastName, color: selectedColor },
+          { withCredentials: true }
+        );
+
+        if (res.status === 200 && res.data) {
+          setUserInfo({ ...res.data });
+          toast.success("Profile updated successfully.");
+          navigate("/chat");
+        }
+      } catch (error) {
+        console.log({ error });
+      }
+    }
+  };
+
+  const handleNavigate = () => {
+    if (userInfo.profileSetup) {
+      navigate("/chat");
+    } else {
+      toast.error("Please setup profile!");
+    }
+  };
+
+  const handleFileInputClick = () => {
+    console.log("In handleFileInputClick");    
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = async (e) => {
+    //! File upload issue
+    console.log("In handleImageChange");    
+    const file = e.target.files[0];
+    console.log({ file });
+    if (file) {      
+      const formData = new FormData();
+      formData.append("profile-image", file);
+
+      try {
+        const res = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },  
+          withCredentials: true,
+        });
+        console.log(res);
+  
+        if (res.status === 200 && res.data.image) {
+          setUserInfo({ ...userInfo, image: res.data.image });
+          toast.success("Image updated successfully.");
+        }        
+      } catch (error) {
+        console.log({error});        
+      }
+
+    }
+  };
+
+  const handleImageDelete = async () => {
+    try {
+      const res = await apiClient.delete(REMOVE_PROFILE_IMAGE_ROUTE, {withCredentials: true});
+      console.log(res);
+      
+      if(res.status === 200) {
+        setUserInfo({...userInfo, image: null});
+        toast.success("Image removed successfully.");
+      }
+    } catch (error) {
+      console.log({error});      
+    }
+  };
 
   return (
     <div className="h-[100vh] flex items-center justify-center flex-col gap-10 bg-[#1b1c24]">
-      <div className="">
+      <div className="flex flex-col gap-7">
         <div>
-          <IoArrowBack className="text-4xl text-white cursor-pointer lg:text-6xl" />
+          <IoArrowBack
+            onClick={handleNavigate}
+            className="text-4xl text-white cursor-pointer lg:text-6xl"
+          />
         </div>
         <div className="grid grid-cols-2">
           <div
@@ -46,7 +159,10 @@ const Profile = () => {
               )}
             </Avatar>
             {hovered && (
-              <div className="absolute inset-0 flex items-center justify-center bg-black/50 cursor-pointer rounded-full">
+              <div
+                className="absolute inset-0 flex items-center justify-center bg-black/50 cursor-pointer rounded-full"
+                onClick={image ? handleImageDelete : handleFileInputClick}
+              >
                 {image ? (
                   <FaTrash className="text-white cursor-pointer text-3xl" />
                 ) : (
@@ -54,7 +170,14 @@ const Profile = () => {
                 )}
               </div>
             )}
-            {/* <input type="text" /> */}
+            <input
+              type="file"
+              ref={fileInputRef}
+              onClick={handleImageChange}
+              className="hidden"
+              accept=".png, .jpg, .jpeg, .svg, .webp"
+              name="profile-image"
+            />
           </div>
           <div className="flex items-center justify-center min-w-32 md:min-w-64 text-white flex-col gap-5">
             <div className="w-full">
@@ -71,7 +194,7 @@ const Profile = () => {
                 placeholder="First name"
                 type="text"
                 value={firstName}
-                onChange={(e) => setLastName(e.target.value)}
+                onChange={(e) => setFirstName(e.target.value)}
                 className="bg-[#2c2e3b] p-6 rounded-lg border-none"
               />
             </div>
@@ -99,6 +222,12 @@ const Profile = () => {
             </div>
           </div>
         </div>
+        <Button
+          className="bg-purple-500 hover:bg-purple-600 rounded-xl w-full"
+          onClick={saveChanges}
+        >
+          Save Changes
+        </Button>
       </div>
     </div>
   );
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,7 @@
 import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
+import {rename, renameSync, unlinkSync} from "fs";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
@@ -95,6 +96,85 @@ export const getUserInfo = async (req, res) => {
       color: userData.color,
       profileSetup: userData.profileSetup,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { userID } = req;
+    const { firstName, lastName, color } = req.body;
+    if (!firstName || !lastName)
+      res.status(400).send("First name, last name and color are required!");
+
+    const userData = await User.findByIdAndUpdate(
+      userID,
+      { firstName, lastName, color, profileSetup: true },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      id: userData._id,
+      email: userData.email,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+      profileSetup: userData.profileSetup,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const addProfileImage = async (req, res) => {
+  try {
+
+      console.log(req.file);
+      
+      if(!req.file) {
+        return res.status(400).send("File not present!");
+      }
+
+      const date = Date.now();
+      const fileName = "uploads/profiles/" + date + req.file.originalname;
+      
+      renameSync(req.file.path, fileName);
+
+      const updatedUserData = await User.findByIdAndUpdate(req.userID, {image: fileName},
+        { new: true, runValidators: true });
+
+    return res.status(200).json({
+      id: updatedUserData._id,
+      image: updatedUserData.image,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const removeProfileImage = async (req, res) => {
+  try {  
+
+    const {userID} = req;
+
+    const user = await User.findById(userID);
+
+    console.log(user);
+    if(!user) return res.status(404).send("User not found!");    
+
+    if(user.image) {
+      unlinkSync(user.image); 
+    }
+    
+    user.image = null;
+    await user.save();
+
+    return res.status(200).send("Profile image deleted successfully");    
   } catch (error) {
     console.log(error);
     return res.status(500).send("Internal Server Error");
